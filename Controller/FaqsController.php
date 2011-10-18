17fac16fba19b06d8f944522ee7c4df524d2786e
Richard Kersey <github@razorit.com>
2011-10-18 14:23:03 -0400

    ->data renamed to ->request->data throughout, and Helper file names updated.

--- Controller/FaqsController.php
+++ Controller/FaqsController.php
@@ -17,9 +17,9 @@ class FaqsController extends AppController {
 	}
 
 	function add() {
-		if (!empty($this->data)) {
+		if (!empty($this->request->data)) {
 			$this->Faq->create();
-			if ($this->Faq->save($this->data)) {
+			if ($this->Faq->save($this->request->data)) {
 				$this->Session->setFlash(__('The faq has been saved', true));
 				$this->redirect(array('action' => 'index'));
 			} else {
@@ -48,20 +48,20 @@ class FaqsController extends AppController {
 		return $faqList;
 	}
 	/*function edit($id = null) {
-		if (!$id && empty($this->data)) {
+		if (!$id && empty($this->request->data)) {
 			$this->Session->setFlash(__('Invalid faq', true));
 			$this->redirect(array('action' => 'index'));
 		}
-		if (!empty($this->data)) {
-			if ($this->Faq->save($this->data)) {
+		if (!empty($this->request->data)) {
+			if ($this->Faq->save($this->request->data)) {
 				$this->Session->setFlash(__('The faq has been saved', true));
 				$this->redirect(array('action' => 'index'));
 			} else {
 				$this->Session->setFlash(__('The faq could not be saved. Please, try again.', true));
 			}
 		}
-		if (empty($this->data)) {
-			$this->data = $this->Faq->read(null, $id);
+		if (empty($this->request->data)) {
+			$this->request->data = $this->Faq->read(null, $id);
 		}
 		$creators = $this->Faq->Creator->find('list');
 		$modifiers = $this->Faq->Modifier->find('list');
@@ -94,9 +94,9 @@ class FaqsController extends AppController {
 	}
 
 	function admin_add() {
-		if (!empty($this->data)) {
+		if (!empty($this->request->data)) {
 			$this->Faq->create();
-			if ($this->Faq->save($this->data)) {
+			if ($this->Faq->save($this->request->data)) {
 				$this->Session->setFlash(__('The faq has been saved', true));
 				$this->redirect(array('action' => 'index'));
 			} else {
@@ -108,20 +108,20 @@ class FaqsController extends AppController {
 	}
 
 	function admin_edit($id = null) {
-		if (!$id && empty($this->data)) {
+		if (!$id && empty($this->request->data)) {
 			$this->Session->setFlash(__('Invalid faq', true));
 			$this->redirect(array('action' => 'index'));
 		}
-		if (!empty($this->data)) {
-			if ($this->Faq->save($this->data)) {
+		if (!empty($this->request->data)) {
+			if ($this->Faq->save($this->request->data)) {
 				$this->Session->setFlash(__('The faq has been saved', true));
 				$this->redirect(array('action' => 'index'));
 			} else {
 				$this->Session->setFlash(__('The faq could not be saved. Please, try again.', true));
 			}
 		}
-		if (empty($this->data)) {
-			$this->data = $this->Faq->read(null, $id);
+		if (empty($this->request->data)) {
+			$this->request->data = $this->Faq->read(null, $id);
 		}
 		$faqTypes = $this->Faq->FaqType->find('list');
 		$this->set(compact('faqTypes'));
